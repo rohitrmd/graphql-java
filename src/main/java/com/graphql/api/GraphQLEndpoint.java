6c9aece8a0150e4190205bf6a02e97f5a403489d
rohitmdesh <rdeshpande@salesforce.com>
2017-10-21 23:01:36 -0700

    added mongodb connector

--- src/main/java/com/graphql/api/GraphQLEndpoint.java
+++ src/main/java/com/graphql/api/GraphQLEndpoint.java
@@ -2,17 +2,24 @@ package com.graphql.api;
 import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
+    private static final LinkRepository linkRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient().getDatabase("graphql");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
                 .file("schema.graphqls")
                 .resolvers(new Query(linkRepository), new Mutation(linkRepository))
